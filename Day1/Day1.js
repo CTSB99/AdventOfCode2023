63f7c88e5a98e2722439172caed3de6dd7ab900a
Christian Schulze <Christian.Schulze@softwareone.com>
2023-12-02 23:31:07 +0100

    updated var name

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -9,14 +9,14 @@ const inputString = inputArray.toString();
 
 for (let i = 0; i < inputArray.length; i++){
   let element = inputArray[i].trim();
-  let array1 = [];
+  let resultArray = [];
 
   for (let k = 0; k < element.length; k++){
     if (!isNaN(element[k])){
-      array1.push(element[k]);
+      resultArray.push(element[k]);
     }
   }
-  let resultString = array1[0] + array1[array1.length-1];
+  let resultString = resultArray[0] + resultArray[resultArray.length-1];
 
   console.log("Line " + i + ": " + resultString);
   result += parseInt(resultString);
