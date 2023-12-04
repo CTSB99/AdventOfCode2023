1db749d043e08a584c7461eef2fbbed68d3a9651
Christian Schulze <Christian.Schulze@softwareone.com>
2023-12-04 10:30:51 +0100

    find numbers and save with x and y index and length

--- Day3/Day3.js
+++ Day3/Day3.js
@@ -4,6 +4,37 @@ const inputArray = fs.readFileSync('./input.txt', { encoding: "utf-8" }) // read
   .toString('UTF8')
   .split('\n');
 
+for (let i = 0; i < inputArray.length; i++){
+  let element = inputArray[i].trim();
+  let result = [];
+  let match;
+  let k = 0;
+  let nums = [];
+  
+  const numsRE = element.match(/\d+/g);
+
+  if(numsRE != null){
+    nums = Array.from(numsRE);
+  }
+
+  console.log(nums);
+
+  var re = /\d+/g;
+  while ((match = re.exec(element)) != null) {
+      console.log(`match ${nums[k]} with length ${nums[k].length} found at ${match.index} to ${match.index + nums[k].length-1}`);
+
+      result = []
+
+      result.push(nums[k]);
+      result.push(i);
+      result.push(match.index);
+      result.push(nums[k].length);
+
+      console.log(result);
+      k++;
+  }
+  k = 0;
+}
 
 /*
 Find Number
