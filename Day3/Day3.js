853953bb0af940ca75e4b51038b5d7c0cc1ecddb
Christian Schulze <Christian.Schulze@softwareone.com>
2023-12-04 09:10:17 +0100

    Day 3 boilerplate

---
+++ Day3/Day3.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+
+const inputArray = fs.readFileSync('./input.txt', { encoding: "utf-8" }) // read input.txt content
+  .toString('UTF8')
+  .split('\n');
+
+
+/*
+Find Number
+Detect if valid
+Add numbers
+*/
