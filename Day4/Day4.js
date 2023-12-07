50515f55d393b0c5816627436e399b2c966d67bc
Christian Schulze <Christian.Schulze@softwareone.com>
2023-12-07 16:43:49 +0100

    Day4 start

--- Day4/Day4.js
+++ Day4/Day4.js
@@ -3,3 +3,18 @@ import fs from 'fs';
 const inputArray = fs.readFileSync('./input.txt', { encoding: "utf-8" }) // read input.txt content
   .toString('UTF8')
   .split('\n');
+
+let resultArray = [];
+let result = [];
+
+inputArray.forEach(element => {
+element = element.split(/[:|]/);
+element.shift();
+console.log(element);
+resultArray.push(element);
+});
+
+console.log(resultArray);
+
+resultArray.forEach(element => {
+});
